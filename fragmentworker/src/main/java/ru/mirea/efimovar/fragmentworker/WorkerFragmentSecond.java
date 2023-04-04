85e4fb97924242aab13b1ae689de7c6dcc971b3a
Efimov-Alex <fastic2018l@yandex.ru>
2023-04-04 18:51:02 +0300

    lesson4 1

--- fragmentworker/src/main/java/ru/mirea/efimovar/fragmentworker/WorkerFragmentSecond.java
+++ fragmentworker/src/main/java/ru/mirea/efimovar/fragmentworker/WorkerFragmentSecond.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,8 @@ public class WorkerFragmentSecond extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    static final String TAG = "WorkerFragmentSecond";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -44,6 +47,20 @@ public class WorkerFragmentSecond extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "WorkFragmentSecond Start");
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "WorkFragmentSecond Destroy");
+
     }
 
     @Override
